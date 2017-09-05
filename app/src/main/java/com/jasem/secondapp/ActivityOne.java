7beec00dd9c271f7376697d1a9d9b4e84def8501
jasemreyes <31647205+jasemreyes@users.noreply.github.com>
2017-09-05 22:38:16 +0800

    Initial commit

---
+++ app/src/main/java/com/jasem/secondapp/ActivityOne.java
@@ -0,0 +1,21 @@
+package com.jasem.secondapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class ActivityOne extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_one);
+
+    }
+
+    public void callAct1App1 (View view){
+        Intent intent = new Intent("com.jasem.firstapp.ActivityTwo");
+        startActivity(intent);
+    }
+}
